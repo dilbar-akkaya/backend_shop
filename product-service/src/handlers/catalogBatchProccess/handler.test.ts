1ec62d37a91269a43bc1a4b0429ef3adad873393
Dilbar Akkaya <dilbar_akkaya@epam.com>
2024-05-22 17:18:30 +0300

    fix: fix tests bugs

--- product-service/src/handlers/catalogBatchProccess/handler.test.ts
+++ product-service/src/handlers/catalogBatchProccess/handler.test.ts
@@ -1,127 +1,52 @@
-import AWSMock from "aws-sdk-mock";
-import AWS from "aws-sdk";
-import { IProduct } from "../../types/products";
-import { v4 as uuidv4 } from "uuid";
-import { SQSEvent } from "aws-lambda";
+import { DynamoDBDocumentClient, TransactWriteCommand } from "@aws-sdk/lib-dynamodb";
+import { SNSClient, PublishCommand, PublishCommandOutput } from "@aws-sdk/client-sns";
+import { mockClient } from "aws-sdk-client-mock";
 import { catalogBatchProccess } from "./handler";
+import { SQSEvent } from "aws-lambda";
 
-AWSMock.setSDKInstance(AWS);
+const mockDynamoDB = mockClient(DynamoDBDocumentClient);
+const mockSNS = mockClient(SNSClient);
 
 describe("catalogBatchProccess", () => {
+    beforeEach(() => {
+        mockDynamoDB.reset();
+        mockSNS.reset();
+    });
 
-    it("should publish message to SNS", async () => {
-
-        const mockeryDynamoDB = jest.fn((_params: unknown, callback: (err: Error | null, data: IProduct | null) => void) => {
-            console.log("Trans was called");
-            const product: IProduct = {
-                title: "Test1",
-                description: "Test1 Description",
-                price: 100,
-                id: uuidv4(),
-            };
-            callback(null, product);
-        });
-        
-        AWSMock.mock("DynamoDB.DocumentClient", "transactWrite", mockeryDynamoDB);
-
-        const mockerySNS = jest.fn();
-
-        AWSMock.mock("SNS", "publish", mockerySNS);
-        
-        const newProduct = {
-            title: "Test1",
-            description: "Test1 Description",
-            price: 100,
-            count: 1
-        };
-        const event: SQSEvent = {
+    it("should add product to DynamoDB and publish SNS", async () => {
+        const mockEvent = {
             Records: [
-                {
-                    messageId: "059f36b4-87a3-44ab-83d2-661975830a7d",
-                    receiptHandle: "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
-                    body: JSON.stringify(newProduct),
-                    attributes: {
-                        "ApproximateReceiveCount": "1",
-                        "SentTimestamp": "1545082649183",
-                        "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                        "ApproximateFirstReceiveTimestamp": "1545082649185"
-                    },
-                    messageAttributes: {},
-                    md5OfBody: "e4e68fb7bd0e697a0ae8f1bb342846b3",
-                    eventSource: "aws:sqs",
-                    eventSourceARN: "arn:aws:sqs:eu-west-1:637423586618:catalogItemsQueue",
-                    awsRegion: "eu-west-1"
-                }
+                { body: JSON.stringify({ title: "Test product", description: "Test", price: 100, count: 10 }) }
             ]
         };
+        const mockDynamoDBResponse = { ConsumedCapacity: [] };
+        const mockSNSResponse: PublishCommandOutput = {
+            $metadata: {
+                httpStatusCode: 200,
+                requestId: "test-id",
+                extendedRequestId: "test-id",
+                cfId: undefined,
+                attempts: 0,
+                totalRetryDelay: 0
+            },
+            MessageId: "test message"
+        };
 
-        await catalogBatchProccess(event);
+        mockDynamoDB.on(TransactWriteCommand).resolves(mockDynamoDBResponse);
+        mockSNS.on(PublishCommand).resolves(mockSNSResponse);
 
-        expect(mockeryDynamoDB).toHaveBeenCalledTimes(1);
-        expect(mockerySNS).toHaveBeenCalledTimes(1);
+        await catalogBatchProccess(mockEvent as SQSEvent);
 
-        AWSMock.restore("DynamoDB.DocumentClient");
-        AWSMock.restore("SNS");
+        expect(mockDynamoDB.calls()).toHaveLength(1);
+        expect(mockSNS.calls()).toHaveLength(1);
     });
+
+    it("should throw error if records is empty", async () => {
+        const mockEvent = { Records: [] };
+
+        await expect(catalogBatchProccess(mockEvent as SQSEvent))
+            .rejects
+            .toThrow("Event doesn't have records");
+    });
+
 });
-/*
-import { SQSEvent } from "aws-lambda";
-import { catalogBatchProccess } from "./handler";
-
-const mockDynamoDBClient = jest.fn(() => ({
-    send: jest.fn()
-}));
-const mockSNSClient = jest.fn(() => ({
-    send: jest.fn()
-}));
-jest.mock("@aws-sdk/client-dynamodb", () => ({
-    DynamoDBClient: mockDynamoDBClient
-}));
-jest.mock("@aws-sdk/client-sns", () => ({
-    SNSClient: mockSNSClient
-}));
-
-it("should publish message to SNS", async () => {
-
-    const sendMockTransact = jest.fn();
-    const sendMockPublish = jest.fn();
-
-    mockDynamoDBClient.mockReturnValue({
-        send: sendMockTransact
-    });
-
-    mockSNSClient.mockReturnValue({
-        send: sendMockPublish
-    });
-
-    const newProduct = {
-        title: "Test1",
-        description: "Test1 Description",
-        price: 100,
-        count: 1
-    };
-    const event: SQSEvent = {
-        Records: [
-            {
-                messageId: "059f36b4-87a3-44ab-83d2-661975830a7d",
-                receiptHandle: "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
-                body: JSON.stringify(newProduct),
-                attributes: {
-                    "ApproximateReceiveCount": "1",
-                    "SentTimestamp": "1545082649183",
-                    "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                    "ApproximateFirstReceiveTimestamp": "1545082649185"
-                },
-                messageAttributes: {},
-                md5OfBody: "e4e68fb7bd0e697a0ae8f1bb342846b3",
-                eventSource: "aws:sqs",
-                eventSourceARN: "arn:aws:sqs:eu-west-1:637423586618:catalogItemsQueue",
-                awsRegion: "eu-west-1"
-            }
-        ]
-    };
-    await catalogBatchProccess(event);
-
-    expect(sendMockTransact).toHaveBeenCalledTimes(1); 
-    expect(sendMockPublish).toHaveBeenCalledTimes(1);
-});*/
--- product-service/src/handlers/catalogBatchProccess/handler.ts
+++ product-service/src/handlers/catalogBatchProccess/handler.ts
@@ -74,3 +74,4 @@ export const catalogBatchProccess = async (event: SQSEvent) => {
 
     };
 };
+
